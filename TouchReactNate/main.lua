1df2405a70c94f979d1f8e5b7922323aa7ea6351
Nate Day <nate.day@ocsbstudent.ca>
2019-10-02 14:34:39 -0400

    updated

--- TouchReactNate/main.lua
+++ TouchReactNate/main.lua
@@ -5,26 +5,67 @@
 
 
 -- set background colour
-display.setDefault ("background", 0/255, 0/255, 0/255)
+display.setDefault ("background", 204/255, 128/255, 0/255)
 
 -- hide status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- create circle button, set its position and make it visible
-local circleButton = display.newImageRect("Images/collisioncircle.png", 198, 96)
-circleButton.x = display.contentWidth/2
-circleButton.y = display.contentHeight/2
-circleButton.isVisible = true
+-- create blue button, set its position and make it visible
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true
 
--- create zero button, set its position and make it invisible
-local zeroButton = display.newImageRect("Images/0.png", 198, 96)
-zeroButton.x = display.contentWidth/2
-zeroButton.y = display.contentHeight/2
-zeroButton.isVisible = false
+-- create red button, set its position and make it invisible
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false
 
 -- create text object, set its position and make it invisible
 local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
 textObject.x = display.contentWidth/2
 textObject.y = display.contentHeight/3
-textObject:setTextColor (0, 0, 0)
+textObject:setTextColor (1, 1, 0)
 textObject.isVisible = false
+
+-- Function: BlueButtonListener
+-- Input: touch listener
+-- Output: none
+-- Description: when blue button is clicked, it will make the text appear with the red button,
+-- and make the blue button disappear
+local function BlueButtonListener(touch)
+	if (touch.phase == "began") then
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+	end
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+    end
+end
+
+-- add the respective listeners to each object
+blueButton:addEventListener("touch", BlueButtonListener)
+
+local function RedButtonListener2(touch)
+	if (touch.phase == "began") then
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+	end
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+    end
+end
+
+
+
+-- add the respective listeners to each object
+redButton:addEventListener("touch", RedButtonListener2)
