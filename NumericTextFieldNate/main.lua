9ef334c4671860ff38c95d6a66f7930cb86f1193
Nate Day <nate.day@ocsbstudent.ca>
2019-10-04 17:12:11 -0400

    update

--- NumericTextFieldNate/main.lua
+++ NumericTextFieldNate/main.lua
@@ -23,7 +23,14 @@ local numericField
 local randomNumber1
 local randomNumber2
 local userAnswer
-local correctAnswer
+local correctAnswer = 0
+local incorrectObject
+local points = 0
+local pointsText
+local correctAnswerObject
+local lives = 3
+local livesText
+
 
 
 ---------------------------------------------------------------------------------------------------------------------------
@@ -36,13 +43,20 @@ local function AskQuestion()
 	randomNumber1 = math.random(0, 10)
 	randomNumber2 = math.random(0, 10)
 
+	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
 	correctAnswer = randomNumber1 + randomNumber2
-	
-	correctAnswer = randomNumber1 .. " + " .. randomNumber2 .. " + "
 end
 
 local function HideCorrect()
 	correctObject.isVisible = false
+	AskQuestion()
+end
+
+-- set the answer (when wrong) to display incorrect
+local function Hideincorrect()
+	incorrectObject.isVisible = false
+	correctAnswerObject.isVisible = false
 	AskQuestion()
 end
 
@@ -54,7 +68,7 @@ local function NumericFieldListener( event )
 		-- clear text 
 		event.target.text = ""
 
-	elseif event.phase == "submitted" then 
+	elseif (event.phase == "submitted") then 
 	
 	    -- when the answer is submitted	set their input to userAnswer
 	    userAnswer = tonumber(event.target.text)
@@ -63,16 +77,36 @@ local function NumericFieldListener( event )
 	    if (userAnswer == correctAnswer) then
 		   correctObject.isVisible = true
 		   timer.performWithDelay(2000, HideCorrect)
+		   	       -- give a point if user gets the correct answer
+	       points = points + 1
+           -- upadate it in the display object
+		   pointsText.text = "Points = " .. points
+		   
+		else
+			incorrectObject.isVisible = true
+		    timer.performWithDelay(2000, Hideincorrect) 
+		    correctAnswerObject = display.newText( " The correct answer is " .. correctAnswer .. "!", 512, 680, nil, 50)
+		   	correctAnswerObject.isVisible = true
+		   	-- take a life if user gets the incorrect answer
+	       lives = lives - 1
+           -- upadate it in the display object
+		   livesText.text = "Lives = " .. lives
 		end
+		    -- clear text 
+		    event.target.text = ""
+		
+		
 	end
 end
 
+      
 
 -------------------------------------------------------------------------------------------------------------
 -- OBJECT CREATION
 -------------------------------------------------------------------------------------------------------------
 
-
+-- display the amount of points as a text object
+pointsText = display.newText("Points = " .. points, 150, 50, nil, 50)
 -- displays a question and sets the colour
 questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50)
 questionObject:setTextColor(.5, .3, .8)
@@ -82,6 +116,11 @@ correctObject = display.newText( "Correct!", display.contentWidth/2, display.con
 correctObject:setTextColor(.2, 1, 0)
 correctObject.isVisible = false
 
+-- create the incorrect text object and make it invisible
+incorrectObject = display.newText( "Incorrect!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+incorrectObject:setTextColor(1, 0, 0)
+incorrectObject.isVisible = false
+
 -- create numeric field 
 numericField = native.newTextField(display.contentWidth/2, display.contentHeight/2, 150, 80)
 numericField.inputType = "number"
@@ -89,6 +128,8 @@ numericField.inputType = "number"
 -- add the event listener for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener )
 
+-- display the amount of lives as a text object
+livesText = display.newText("Lives = " .. lives, 150, 100, nil, 50)
 
 ------------------------------------------------------------------------------------------------------------
 -- FUNCTION CALLS
@@ -99,7 +140,7 @@ AskQuestion()
 
 
 	   
-	  
+  -- Day 17 added code
 
 
  
