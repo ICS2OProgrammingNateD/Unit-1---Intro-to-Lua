9b4293d8e8f1eca50a0eab2153ad6238b333b317
Nate Day <nate.day@ocsbstudent.ca>
2019-10-17 14:12:35 -0400

    added physics and collisions

---
+++ Physics&CollisionsNateD/main.lua
@@ -0,0 +1,61 @@
+-- Title: PhysicsAndCollisions
+-- Name: Nate Day
+-- Course: ICS2O
+-- This program is made to have objects interact using physics.
+
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+
+
+
+--------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+--------------------------------------------------------------------------------------
+
+local backgroundImage
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+---------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------
+-- OBJECT CREATION
+--------------------------------------------------------------------------------------------
+
+backgroundImage = display.newImageRect("Images/background.jpg", 1304, 769)
+backgroundImage.x = display.contentCenterX
+backgroundImage.y = display.contentCenterY
